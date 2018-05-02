74249d49a17ebcd2af247deea415f6108aa30ff4
Robin van der Vleuten <robin@webstronauts.co>
2018-05-02 15:19:35 +0200

    Correctly handle rejections

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
 require('please-upgrade-node')(require('../package.json'));
 require('../lib')();
